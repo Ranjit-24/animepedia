c756439520d70e9eeb10b999a11fc9f3fb830706
Ranjit k <k.ranjitkumar1479@gmail.com>
2024-05-03 23:28:56 +0530

    video js commit5

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { HashRouter as Router , Routes,Route , Link} from 'react-router-dom'
 import { Manga } from './components/manga'
 import { Animecard } from './components/animedetails'
 import { Home } from './components/home'
-import { Animewatch } from './components/animewatch'
+import { Animewatch } from './components/animewatch_coponents/animewatch'
 import { createContext, useEffect } from 'react'
 import  Axios  from 'axios'
 import { useState } from 'react'
@@ -23,6 +23,7 @@ function App() {
       const resp = await Axios.get(`https://api.jikan.moe/v4/top/${category}?page=${pagenum}`)
       adddata(resp.data.data)
       setfulldata(resp.data.data)
+
     }
     catch(e){
       console.log('fetch',e)
---
+++ src/components/animewatch_coponents/animewatch.jsx
@@ -0,0 +1,68 @@
+import { useState,useContext, useEffect} from "react"
+import { appcontext } from "../../App";
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import './animewatch.css'
+async function stage1_fetch(ani_search){
+    const fetch_data= await fetch( `https://aniwatch-api-v1-0.onrender.com/api/search/${ani_search}`)
+    let data = await  fetch_data.json()
+    return data.searchYour[0].idanime
+}
+async function stage2_fetch(animeid){
+    const fetch_video_url= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/related/${animeid}`)
+    let data= await fetch_video_url.json()
+    data.infoX[3]=== undefined ? animeid=animeid:animeid=data.infoX[3].season[0].id
+    // here the last number is seprated from animeid
+    animeid=animeid.split("-")
+    return animeid[animeid.length-1]
+}
+async function stage3_fetch(animeid,epnum){
+    const fetch_anime_episode_id= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/episode/${animeid}`)
+    let data= await fetch_anime_episode_id.json()
+    let epid =data.episodetown[epnum].epId
+    epid=epid.split("ep=")[1]
+    const fetch_server_id= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/server/${epid}`)
+    data= await fetch_server_id.json()
+    let srcId= data.sub[0].srcId
+    const fetch_url= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/src-server/${srcId}`)
+    data= await fetch_url.json()
+    return data.restres.sources[0].url
+}
+async function fetch_data(ani_search,setwatchload,setvideourl,setloadstatus,animeid,setanimeid){
+    //fetching animeid
+    animeid =await stage1_fetch(ani_search)
+    setloadstatus("stage1 fetching completed")
+    //fetching anime season id
+    animeid =await stage2_fetch(animeid)
+    setloadstatus("Almost there")
+    //fetching episode id
+    //fetching server//fetching url
+    setvideourl(await stage3_fetch(animeid,0));
+    setloadstatus("stage5 fetching completed")
+    setanimeid(animeid)
+    setwatchload(false)
+}
+export function Animewatch(){
+    let [animeid,setanimeid]=useState()
+    let [watchdataloading,setwatchload]=useState(true)
+    let {fullanimedata,showid,loading}=useContext(appcontext)
+    let [video_url,setvideourl]=useState();
+    let [loadingstatus,setloadstatus]=useState("wait for a min");
+    useEffect(()=>{!loading && fetch_data(fullanimedata[showid].title,setwatchload,setvideourl,setloadstatus,animeid,setanimeid)},[])
+    --showid
+    return <div id="aniwatch-div">{watchdataloading ? 
+    <>
+    <h1>Loading.....</h1>
+    <p>{loadingstatus}</p>
+    </>
+    :
+    <>
+    <video ref={videojs} id="my-video"
+            className="video-js"
+            controls
+            poster="MY_VIDEO_POSTER.jpg"
+            data-setup="{}">
+        <source src={`${video_url}`} type="application/x-mpegURL"/>
+    </video>
+    </>}</div>
+}
