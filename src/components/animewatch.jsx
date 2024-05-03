626b31277c39df11e5b70b0760b1a2e1ca7e21c8
Ranjit k <k.ranjitkumar1479@gmail.com>
2024-05-03 20:07:29 +0530

    video js commit4

--- src/components/animewatch.jsx
+++ src/components/animewatch.jsx
@@ -2,6 +2,7 @@ import { useState,useContext, useEffect } from "react"
 import { appcontext } from "../App";
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
+import './animewatch.css'
 async function fetch_data(ani_search,setwatchload,setseason_id){
     let animeid
     //fetching animeid
@@ -12,9 +13,12 @@ async function fetch_data(ani_search,setwatchload,setseason_id){
     //fetching anime season id
     const fetch_anime_season_id= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/related/${animeid}`)
     data= await fetch_anime_season_id.json()
-    data.infoX[3]=== undefined ? animeid=animeid.split("-"):animeid=data.infoX[3].season[0].id
-    // here the last number is seprated from animeid  
-    animeid=animeid[animeid.length-1]  
+    data.infoX[3]=== undefined ? animeid=animeid:animeid=data.infoX[3].season[0].id
+    // here the last number is seprated from animeid
+    animeid=animeid.split("-")
+    console.log(animeid)  
+    animeid=animeid[animeid.length-1]
+    
     console.log("stage2 fetching completed")
     //fetching episode id
     const fetch_anime_episode_id= await fetch(`https://aniwatch-api-v1-0.onrender.com/api/episode/${animeid}`)
@@ -45,12 +49,11 @@ export function Animewatch(){
     //reload issue solution 
     return <div id="aniwatch-div">{watchdataloading ? <h1>Loading.....</h1>:
     <>
-    <p>{anime_season_id}</p>
     <video ref={videojs} id="my-video"
+            width="800px"
+            height="300px"
             className="video-js"
             controls
-            width="640"
-            height="264"
             poster="MY_VIDEO_POSTER.jpg"
             data-setup="{}">
         <source src={`${anime_season_id}`} type="application/x-mpegURL"/>
